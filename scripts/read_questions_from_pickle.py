302a495968fbdf38a08e30a085aaf03693211cb2
Joe <j176665@dac.unicamp.br>
2019-04-25 02:42:38 -0300

    Added script read_questions_from_pickle.py used to read questions inside pickle file.

---
+++ scripts/read_questions_from_pickle.py
@@ -0,0 +1,47 @@
+import pickle
+import requests
+
+class Question:
+	"""
+	The URI python class
+
+	Atributes:
+		name (str): Problem name
+		number (int): Problem number id
+		input (str): Problem input description
+		output (str): Problem output description
+		timelimit (float): Problem limit of execution
+		description (str): Problem main description
+	"""
+
+	def __init__(self):
+		self.id = 0
+		self.name = ""
+		self.input = ""
+		self.level = ""
+		self.author = ""
+		self.output = ""
+		self.category = ""
+		self.timelimit = 0.0
+		self.description = ""
+
+	def __repr__(self):
+		str = "Questions attributes("
+		str = str + ("id=%d" % self.id) + ","
+		str = str + ("name='%s'" % self.name) + ","
+		str = str + ("author='%s'" % self.author) + ","
+		str = str + ("description='%s'" % self.description) + ","
+		str = str + ("input='%s'" % self.input) + ","
+		str = str + ("output='%s'" % self.output) + ","
+		str = str + ("level='%s'" % self.level) + ","
+		str = str + ("category='%s'" % self.category) + ","
+		str = str + ("timelimit=%f" % self.timelimit) + ")"
+		return str
+
+file = open('../data/questions.pickle', 'rb')
+
+questions = pickle.load(file)
+
+for q in questions:
+	print(q)
+	print("***********************")
